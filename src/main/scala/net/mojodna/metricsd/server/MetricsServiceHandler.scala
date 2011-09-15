227b09e36a7ca437f8d0de96d0982e181036c029
Seth Fitzsimmons <seth@mojodna.net>
2011-09-14 19:09:10 -0600

    Use a Regex to match metrics strings
    Also fixes a bug where the sample rate syntax was wrong (missing @).

--- src/main/scala/net/mojodna/metricsd/server/MetricsServiceHandler.scala
+++ src/main/scala/net/mojodna/metricsd/server/MetricsServiceHandler.scala
@@ -6,9 +6,10 @@ import org.jboss.netty.channel.{ExceptionEvent, MessageEvent, ChannelHandlerCont
 import com.yammer.metrics.core.MetricName
 import java.util.concurrent.TimeUnit
 import com.yammer.metrics.Metrics
+import util.matching.Regex
 
 /**
- * A service handler for :-delimited metrics (à la Etsy's statsd).
+ * A service handler for :-delimited metrics strings (à la Etsy's statsd).
  */
 class MetricsServiceHandler
   extends SimpleChannelUpstreamHandler with Logging {
@@ -18,56 +19,55 @@ class MetricsServiceHandler
   val METER_METRIC_TYPE = "m"
   val TIMER_METRIC_TYPE = "ms"
 
+  val MetricMatcher = new Regex("""([^:]+)(:((\d+)?(\|((\w+)(\|@(\d+\.\d+))?)?)?)?)?""")
+
   override def messageReceived(ctx: ChannelHandlerContext, e: MessageEvent) = {
     val msg = e.getMessage.asInstanceOf[String]
 
     log.trace("Received message: %s", msg)
 
-    // parse message and do stuff
-    val messageParts = msg.split(":")
-    val metricName = messageParts(0).trim.replaceAll("\\s+", "_").replaceAll("\\/", "-").replaceAll("[^a-zA-Z_\\-0-9\\.]", "")
+    // parse message
+    val MetricMatcher(_metricName, _, _, _value, _, _, _metricType, _, _sampleRate) = msg.trim
 
-    messageParts.length match {
-      case 1 =>
-        // TODO remove redundancy
-        log.trace("Marking meter '%s'", metricName)
+    // clean up the metric name
+    val metricName = _metricName.replaceAll("\\s+", "_").replaceAll("\\/", "-").replaceAll("[^a-zA-Z_\\-0-9\\.]", "")
+
+    val metricType = if (_value == null) {
+      METER_METRIC_TYPE
+    } else if (_metricType == null) {
+      COUNTER_METRIC_TYPE
+    } else {
+      _metricType
+    }
+
+    val value : Long = if (_value != null) {
+      _value.toLong
+    } else {
+      1 // meaningless value
+    }
+
+    val sampleRate : Double = if (_sampleRate != null) {
+      _sampleRate.toDouble
+    } else {
+      1.0
+    }
+
+    metricType match {
+      case COUNTER_METRIC_TYPE =>
+        log.debug("Incrementing counter '%s' with %d at sample rate %f (%d)", metricName, value, sampleRate, round(value * 1 / sampleRate))
+        Metrics.newCounter(new MetricName("metrics", "counter", metricName)).inc(round(value * 1 / sampleRate))
+
+      case HISTOGRAM_METRIC_TYPE | TIMER_METRIC_TYPE =>
+        log.debug("Updating histogram '%s' with %d", metricName, value)
+        // note: assumes that values have been normalized to integers
+        Metrics.newHistogram(new MetricName("metrics", "histogram", metricName)).update(value)
+
+      case METER_METRIC_TYPE =>
+        log.debug("Marking meter '%s'", metricName)
         Metrics.newMeter(new MetricName("metrics", "meter", metricName), "samples", TimeUnit.SECONDS).mark
 
-      case _ =>
-        val valueParts = messageParts(1).trim.split("\\|")
-        val value = valueParts(0).toLong
-
-        // extract the metric type
-        val metricType = if (valueParts.length > 1) {
-          valueParts(1)
-        } else {
-          COUNTER_METRIC_TYPE
-        }
-
-        metricType match {
-          case COUNTER_METRIC_TYPE =>
-            // extract the sample rate
-            val sampleRate = if (valueParts.length > 2) {
-              valueParts(2).toFloat
-            } else {
-              1.0
-            }
-
-            log.trace("Incrementing counter '%s' with %d at sample rate %f (%d)", metricName, value, sampleRate, round(value * 1 / sampleRate))
-            Metrics.newCounter(new MetricName("metrics", "counter", metricName)).inc(round(value * 1 / sampleRate))
-
-          case HISTOGRAM_METRIC_TYPE | TIMER_METRIC_TYPE =>
-            log.trace("Updating histogram '%s' with %d", metricName, value)
-            // note: assumes that values have been normalized to integers
-            Metrics.newHistogram(new MetricName("metrics", "histogram", metricName)).update(value)
-
-          case METER_METRIC_TYPE =>
-            log.trace("Marking meter '%s'", metricName)
-            Metrics.newMeter(new MetricName("metrics", "meter", metricName), "samples", TimeUnit.SECONDS).mark
-
-          case x: String =>
-            log.error("Unknown metric type: %s", x)
-        }
+      case x: String =>
+        log.error("Unknown metric type: %s", x)
     }
 
     Metrics.newMeter(new MetricName("metricsd", "meter", "samples"), "samples", TimeUnit.SECONDS).mark
